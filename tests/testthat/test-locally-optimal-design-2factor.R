3a741e858aa0c96bfb9c58eeb2b35ffccc26c3bf
hoanglinh171 <hoailinh.hoang17@gmail.com>
2026-08-31 01:04:03 +0800

    vignette and complete package

--- tests/testthat/test-locally-optimal-design-2factor.R
+++ tests/testthat/test-locally-optimal-design-2factor.R
@@ -2,7 +2,7 @@ design_info <- set_design_info(k_levels=3, j_factor=2, n_unit=170,
                                censor_time=1000, p=0.001, use_cond=c(0, 0), sigma=0.6734, 
                                reparam=FALSE)
 
-pso_info <- pso_setting(n_swarm=16, max_iter=30, early_stopping=50, 
+pso_info <- pso_setting(n_swarm=16, max_iter=30, early_stopping=10, 
                         tol=0.0001)
 
 set.seed(5)
